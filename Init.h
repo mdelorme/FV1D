9e810514d0b0f10e4b81ea66b7179720769cf24c
Maxime Delorme <maxime.delorme@cea.fr>
2023-06-15 11:05:52 +0200

    Corrected HSE init + implemented 1D restart

--- Init.h
+++ Init.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <fstream>
+
 #include "SimInfo.h"
 #include "BoundaryConditions.h"
+
 
 
 namespace fv1d {
@@ -63,19 +66,16 @@ void init_C91(Array &Q) {
 }
 
 void init_C91_hse(Array &Q) {
-  /**
-   * NON FUNCTIONAL : TODO Debug this
-   */
   std::cout << "Initializing C91 - HSE" << std::endl;
 
   for (int i=ibeg; i < iend; ++i) {
     real_t x = get_x(i);
     real_t T = (1.0 + theta1*x);
-    real_t rho = (1.0/(theta1*(m1+1))*(  std::pow(1.0+theta1*(x+0.5*dx), m1+1) 
-                                       - std::pow(1.0+theta1*(x-0.5*dx), m1+1)));
+    real_t rho = (1.0/(dx*theta1*(m1+1))*(  std::pow(1.0+theta1*(x+0.5*dx), m1+1) 
+                                          - std::pow(1.0+theta1*(x-0.5*dx), m1+1)));
     
-    real_t prs = (1.0/(theta1*(m1+2))*(  std::pow(1.0+theta1*(x+0.5*dx), m1+2) 
-                                       - std::pow(1.0+theta1*(x-0.5*dx), m1+2)));
+    real_t prs = (1.0/(dx*theta1*(m1+2))*(  std::pow(1.0+theta1*(x+0.5*dx), m1+2) 
+                                          - std::pow(1.0+theta1*(x-0.5*dx), m1+2)));
 
     Q[i][IR] = rho;
     Q[i][IU] = 0.0;
@@ -112,6 +112,31 @@ void init_B02(Array &Q) {
   }
 }
 
+void init_B02_restart(Array &Q) {
+  std::cout << "Restarting B02 from file ics.dat" << std::endl;
+  
+  std::string filename_in = "ics.dat";
+  std::ifstream f_in(filename_in);
+
+  // Skipping first line
+  std::string tmp;
+  std::getline(f_in, tmp);
+ 
+  for (int i=ibeg; i < iend; ++i) {
+    f_in >> tmp >> Q[i][IR] >> Q[i][IU] >> Q[i][IP];
+    constexpr real_t pert_amp = 1.0e-3;
+    real_t pert = ((float)rand() / (float)RAND_MAX - 0.5)*2.0;
+    real_t x = get_x(i);
+    const real_t pert_margin = dx;
+    if (x < pert_margin || x > (b02_xmid-pert_margin))
+      pert = 0.0;
+
+    Q[i][IP] *= (1.0 + pert);
+  }
+  
+  f_in.close();
+}
+
 void init(Array &Q) {
   if (problem == "SOD")
     init_sod(Q);
@@ -123,6 +148,8 @@ void init(Array &Q) {
     init_C91_hse(Q);
   else if (problem == "B02")
     init_B02(Q);
+  else if (problem == "B02_restart")
+    init_B02_restart(Q);
 
   // One time init stuff
   init_boundaries();
